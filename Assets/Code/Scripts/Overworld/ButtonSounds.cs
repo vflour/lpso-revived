30d2537008eb667745fc22e0d07298ecf17e86fb
mack <mariegilbackup@gmail.com>
2023-07-17 15:54:32 +0200

    added sound effects + prefab buttons to main ui

--- Assets/Code/Scripts/Overworld/ButtonSounds.cs
+++ Assets/Code/Scripts/Overworld/ButtonSounds.cs
@@ -7,10 +7,14 @@ public class ButtonSounds : MonoBehaviour
     public AudioSource ButtonSound;
     public AudioClip hoverSound;
     public AudioClip pressedSound;
+    public AudioClip openPDA;
 
     public void playHover()
     { ButtonSound.PlayOneShot(hoverSound); }
 
     public void playClick()
     { ButtonSound.PlayOneShot(pressedSound); }
+
+    public void playPDA()
+    { ButtonSound.PlayOneShot(openPDA); }
 }
