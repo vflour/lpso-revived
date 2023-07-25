d162a153ec9689e3031d8df48953c3b08243f65a
VividReality <thevividreality@gmail.com>
2023-07-25 12:34:18 +0200

    put animated npc in party dasher

---
+++ Assets/Art/Login/Tiles/tiles.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tiles" tilewidth="70" tileheight="67" tilecount="27" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="68" height="49" source="1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="70" height="51" source="2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="68" height="49" source="3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="68" height="49" source="4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="55" height="40" source="5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="55" height="40" source="6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="55" height="40" source="7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="41" height="30" source="8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="41" height="30" source="9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="42" height="31" source="10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="41" height="30" source="11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="68" height="56" source="1-moved-up.png"/>
+ </tile>
+ <tile id="12">
+  <image width="68" height="59" source="1-moved-up2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="70" height="58" source="2-moved-up.png"/>
+ </tile>
+ <tile id="14">
+  <image width="70" height="61" source="2-moved-up2.png"/>
+ </tile>
+ <tile id="15">
+  <image width="68" height="56" source="3-moved-up.png"/>
+ </tile>
+ <tile id="16">
+  <image width="68" height="59" source="3-moved-up2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="68" height="56" source="4-moved-up.png"/>
+ </tile>
+ <tile id="18">
+  <image width="68" height="59" source="4-moved-up2.png"/>
+ </tile>
+ <tile id="19">
+  <image width="68" height="62" source="1-moved-up3.png"/>
+ </tile>
+ <tile id="20">
+  <image width="70" height="64" source="2-moved-up3.png"/>
+ </tile>
+ <tile id="21">
+  <image width="68" height="62" source="3-moved-up3.png"/>
+ </tile>
+ <tile id="22">
+  <image width="68" height="62" source="4-moved-up3.png"/>
+ </tile>
+ <tile id="24">
+  <image width="68" height="62" source="4-moved-up4.png"/>
+ </tile>
+ <tile id="25">
+  <image width="68" height="67" source="4-moved-up5.png"/>
+ </tile>
+ <tile id="26">
+  <image width="68" height="67" source="4-moved-up6.png"/>
+ </tile>
+ <tile id="27">
+  <image width="68" height="67" source="3-moved-up4.png"/>
+ </tile>
+</tileset>
