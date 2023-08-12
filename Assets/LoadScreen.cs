bfaebd89cb03c09346d3cd25f56b98dc5483e9d3
vflour <vflour@email.com>
2023-08-12 00:16:05 -0400

    Add items to loading screen
    - Add a scrollable progress bar to the screen
    - Also adds the funny tile animation

--- Assets/LoadScreen.cs
+++ Assets/LoadScreen.cs
@@ -1,26 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadScreen : MonoBehaviour
 {
-    public GameObject LoadScreenObject;
+    public Slider loadSlider;
+    public float loadingTime = 3f;
+    private float _loadProgress = 0;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        LoadScreenObject.SetActive(true);
         StartCoroutine(Timer());
     }
-
-    IEnumerator Timer()
+    
+    /// <summary>
+    /// Logic for the timer
+    /// </summary>
+    private IEnumerator Timer()
     {
-        float time = 5f;
+        yield return new WaitUntil(() => GameDataManager.Instance.Loaded);
+        float time = loadingTime;
         while (time >= 0)
         {
             time -= .1f;
+            _loadProgress = (loadingTime-time)/loadingTime;
+            t = 0;
             yield return new WaitForSeconds(.1f);
         }
-        LoadScreenObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+    
+    private float t = 0;
+    /// <summary>
+    /// Updates the slider ui when progress is made
+    /// </summary>
+    void Update()
+    {
+        loadSlider.value = Mathf.Lerp(loadSlider.value, _loadProgress, t);
+        t += 0.75f * Time.deltaTime;
     }
 }
